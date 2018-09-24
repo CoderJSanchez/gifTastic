b110e7322235e3bb0c1bf252267c1b8ae1736a19
julioSanchez <juliosanchez@Julios-MBP.local>
2018-09-24 13:06:22 -0500

    added nav bar with dropdown

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,9 +6,9 @@
 const catagoryArray = ['cats', 'dogs', 'birds'];
 console.log(catagoryArray);
 
-function makeNewButtons(){
+function makeNewButtons() {
     $('#buttonsArea').empty();
-    for(let i = 0; i < catagoryArray.length; i++){
+    for (let i = 0; i < catagoryArray.length; i++) {
         var newButton = $('<button>');
         newButton.attr('data-name', catagoryArray[i]);
         newButton.addClass('btn btn-outline-info catagoryButton')
@@ -20,7 +20,7 @@ function makeNewButtons(){
 
 makeNewButtons();
 
-$('#newGifValue').on('click', function(event){
+$('#newGifValue').on('click', function (event) {
     event.preventDefault();
     var newGif = $('#gifInput').val();
     console.log(newGif);
@@ -29,58 +29,60 @@ $('#newGifValue').on('click', function(event){
     console.log(catagoryArray);
 })
 
-function goGetGif(){
+function goGetGif() {
 
+    $('.catagoryButton').on('click', function () {
+        var searchItem = $(this).attr('data-name');
 
-$('.catagoryButton').on('click', function() {
-    var searchItem = $(this).attr('data-name');
-    
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=rUGDSJQ6ygNJgDewflUlFnfEcjceivwp&q=' + searchItem +'&limit=10&offset=0&rating=G&lang=en'
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=rUGDSJQ6ygNJgDewflUlFnfEcjceivwp&q='
+            + searchItem
+            + '&limit=10&offset=0&rating=G&lang=en'
 
-    $.ajax({
-        url : queryURL,
-        method : 'GET'
-    }).then(function(response) {
-        var returnData = response.data;
-        console.log(response);
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function (response) {
+            var returnData = response.data;
+            console.log(response);
 
-        for (var i = 0; i < returnData.length; i++) {
-            var dropGifs = $('<div>');
-            dropGifs.addClass('col-md-3');
-            var showImage = $('<img>');
-            showImage.addClass('mb-2');
-            showImage.attr('src', returnData[i].images.fixed_height_still.url);
-            showImage.attr('data-still', returnData[i].images.fixed_height_still.url);
-            showImage.attr('data-animate', returnData[i].images.fixed_height.url);
-            showImage.attr('data-state', 'still');
-            showImage.addClass('gif');
-            dropGifs.append(showImage);
+            for (var i = 0; i < returnData.length; i++) {
+                var dropGifs = $('<div>');
+                dropGifs.addClass('col-md-3');
+                var showImage = $('<img>');
+                showImage.addClass('mb-4');
+                showImage.attr('src', returnData[i].images.fixed_height_still.url);
+                showImage.attr('data-still', returnData[i].images.fixed_height_still.url);
+                showImage.attr('data-animate', returnData[i].images.fixed_height.url);
+                showImage.attr('data-state', 'still');
+                showImage.addClass('gif');
+                dropGifs.append(showImage);
 
-            var rating = $('<p>');
-            rating.text('Rating: ' + returnData[i].rating.toUpperCase());
-            dropGifs.prepend(rating);
-            $('#gifArea').append(dropGifs);
-            
-        }
-        $('.gif').on('click', function(){
-            var state = $(this).attr('data-state');
-        
-            if(state === 'still'){
-                $(this).attr('src', $(this).attr('data-animate'));
-                $(this).attr("data-state", "animate");
-            }else{
-                $(this).attr("src", $(this).attr("data-still"));
-                $(this).attr("data-state", "still");
+                var rating = $('<p>');
+                rating.text('Rating: ' + returnData[i].rating.toUpperCase());
+                rating.addClass('mb-0');
+                dropGifs.prepend(rating);
+                $('#gifArea').append(dropGifs);
+
             }
-            
-        })
+            $('.gif').on('click', function () {
+                var state = $(this).attr('data-state');
+
+                if (state === 'still') {
+                    $(this).attr('src', $(this).attr('data-animate'));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+
+            })
+        });
     });
-});
 }
 
 
 
-    
+
 
 
 
