bffa928564e5d91f51d4f946e84f67c74cd791f2
julioSanchez <juliosanchez@Julios-MBP.local>
2018-09-24 08:33:27 -0500

    can add catagories with input, added rating, added still img

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,66 @@
 //rUGDSJQ6ygNJgDewflUlFnfEcjceivwp
 
+
+///////////////////////////////////////////////////////////////
+
 const catagoryArray = ['cats', 'dogs', 'birds'];
 console.log(catagoryArray);
 
-for(let i = 0; i < catagoryArray.length; i++){
-    var newButton = $('<button>');
-    newButton.attr('data-name', catagoryArray[i]);
-    //newButton.attr('type', button);
-    newButton.addClass('btn btn-outline-info')
-    newButton.text(catagoryArray[i]).val();
-    $('#buttonsArea').append(newButton);
+function makeNewButtons(){
+    $('#buttonsArea').empty();
+    for(let i = 0; i < catagoryArray.length; i++){
+        var newButton = $('<button>');
+        newButton.attr('data-name', catagoryArray[i]);
+        newButton.addClass('btn btn-outline-info catagoryButton')
+        newButton.text(catagoryArray[i]).val();
+        $('#buttonsArea').append(newButton);
+    }
 }
 
+makeNewButtons();
 
-function makeButtons(){
+
+$('button').on('click', function() {
+    var searchItem = $(this).attr('data-name');
     
-}
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=rUGDSJQ6ygNJgDewflUlFnfEcjceivwp&q=' + searchItem +'&limit=10&offset=0&rating=G&lang=en'
+
+    $.ajax({
+        url : queryURL,
+        method : 'GET'
+    }).then(function(response) {
+        var returnData = response.data;
+        console.log(response);
+
+        for (var i = 0; i < returnData.length; i++) {
+            var showImage = $('<img>');
+            showImage.addClass('mb-4');
+            showImage.attr('src', returnData[i].images.fixed_height_still.url);
+            showImage.attr('data-animate', returnData[i].images.fixed_height.url);
+            showImage.addClass('gif');
+
+            var rating = $('<p>');
+            rating.addClass('col-md-3');
+            rating.text(returnData[i].rating);
+            //showImage.prepend(rating);
+            $('#gifArea').append(showImage, rating);
+        }
+    });
+});
+
+
+$('#newGifValue').on('click', function(event){
+    event.preventDefault();
+    var newGif = $('#gifInput').val();
+    console.log(newGif);
+    catagoryArray.push(newGif);
+    makeNewButtons();
+})
+    
+
+
+
+
+
+
+
